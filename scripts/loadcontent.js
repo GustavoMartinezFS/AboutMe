1140d912ea75dac660ca839469cd8160e6b7edc3
GustavoMartinezFS <martinez.trejo.gustavo.a@gmail.com>
2020-07-18 14:45:39 -0300

    change directory

--- scripts/loadcontent.js
+++ scripts/loadcontent.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('content.json')
+    fetch('./scripts/content.json')
         .then(response => response.json())
         .then(data => {
             // Home
